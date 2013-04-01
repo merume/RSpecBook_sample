696b172e7b3393f81a303b6c3cf80b47140f190a
Masaki Sato <merume@gmail.com>
2013-04-01 21:27:22 +0900

    Chapter 19 Rails における BDD

---
+++ rails_bdd/showtime/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showtime::Application.config.secret_token = '02f768395bd489175a136f85bf41420f4167f378f35d8149ea128d1af2114454ba13d20d83391c4ea4c309cd180335909e8f1e11604e8bb3935675e87d9cfb9b'
