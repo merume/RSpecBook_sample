554426899a5721e8f821d95b5ccbd78ef985f1e4
Masaki Sato <merume@gmail.com>
2013-04-04 14:14:04 +0900

    Chapter 21 Webrat を使ってブラウザをシミュレートする

--- rails_bdd/showtime/db/schema.rb
+++ rails_bdd/showtime/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130402125223) do
+ActiveRecord::Schema.define(:version => 20130404030229) do
+
+  create_table "genres", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "movies", :force => true do |t|
     t.date     "showtime_date"
