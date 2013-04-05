6a8a5e5ef290f114444427294e77fa915fe29ff5
Masaki Sato <merume@gmail.com>
2013-04-05 15:21:05 +0900

    Chapter 23 Rail ビュー

---
+++ messages/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messages::Application.config.secret_token = '7f98ccd4cec8bafebea5b0f85352211882fd58f65260fd9bf8ea1ee1afe563123adb8b90864ba2b2d81f14847749196503c349cd9c806112bef40ea2d685e50f'
