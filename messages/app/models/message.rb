0b0b9ac691a81562ce3c5a5e4c357037e92a453b
Masaki Sato <merume@gmail.com>
2013-04-06 22:42:14 +0900

    Chapter 25 Rails モデル
    モデルスペックを作成する

--- messages/app/models/message.rb
+++ messages/app/models/message.rb
@@ -1,3 +1,6 @@
 class Message < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :title, :text, :recipient
+  belongs_to :recipient, :class_name => "User"
+
+  validates_presence_of :title, :text, :recipient
 end
